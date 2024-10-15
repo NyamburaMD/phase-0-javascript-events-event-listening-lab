7e61e180ca3ccdf8abf13e9889028ce6a38aecaf
darlenenyambura <darlene.nyambura@student.moringaschool.com>
2024-10-15 16:41:51 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+// index.js
+
 function addingEventListener() {
-}
+    // Select the button element
+    const input = document.getElementById('button');
+  
+    // Define the callback function
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    // Add event listener to the button
+    input.addEventListener('click', clickAlert);
+  }
+  
+  // Call the function to set up the event listener
+  addingEventListener();
+  
